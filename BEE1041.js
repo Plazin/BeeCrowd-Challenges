f2ef142aa73d5b5c415184676d1ca434aabe1329
NandoPla <fernandopla8@gmail.com>
2023-03-16 15:14:44 -0300

    feat: new problems

--- BEE1041.js
+++ BEE1041.js
@@ -1,8 +1,23 @@
 var input = require('fs').readFileSync('./dev/stdin', 'utf8');
 var lines = input.split('\n');
 
-let [x, y] = lines[0].split(" ").map(Number)
+let [x, y] = lines[0].split(" ").map(Number);
 
+if(x > 0 && y > 0){
+  console.log("Q1")
+} else if(x < 0 && y < 0){
+  console.log("Q3")
+} else if(x > 0 && y < 0){
+  console.log("Q4")
+} else if(x < 0 && y > 0){
+  console.log("Q2")
+} else if(x == 0 && y == 0){
+  console.log("Origem")
+} else if(x == 0 && y != 0){
+  console.log("Eixo Y")
+} else if(x =! 0 && y == 0){
+  console.log("Eixo X")
+}
 /* Leia 2 valores com uma casa decimal (x e y), que devem representar as coordenadas de um ponto em um plano. A seguir, determine qual o quadrante ao qual pertence o ponto, ou se está sobre um dos eixos cartesianos ou na origem (x = y = 0).
 
 Se o ponto estiver na origem, escreva a mensagem “Origem”.
---
+++ BEE1042.js
@@ -0,0 +1,25 @@
+var input = require('fs').readFileSync('./dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+let ordemCrescente = lines[0].split(" ").map(Number);
+let ordemLida = lines[0].split(" ").map(Number);
+
+ordemCrescente.sort((a,b) => {
+  if(a > b) return 1;
+  if(a < b) return -1;
+  return 0;
+});
+
+console.log(`${ordemCrescente[0]}\n${ordemCrescente[1]}\n${ordemCrescente[2]}\n`);
+
+console.log(`${ordemLida[0]}\n${ordemLida[1]}\n${ordemLida[2]}`);
+
+
+
+/* Leia 3 valores inteiros e ordene-os em ordem crescente. No final, mostre os valores em ordem crescente, uma linha em branco e em seguida, os valores na sequência como foram lidos.
+
+Entrada
+A entrada contem três números inteiros.
+
+Saída
+Imprima a saída conforme foi especificado.*/
