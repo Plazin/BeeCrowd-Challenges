ecf5b7362b9f0306d15f6a285e691770f5bf7b6f
NandoPla <fernandopla8@gmail.com>
2023-03-16 01:43:38 -0300

    feat: import new font

--- BEE1038.js
+++ BEE1038.js
@@ -1,9 +1,24 @@
 var input = require('fs').readFileSync('./dev/stdin', 'utf8');
-var lines = input.split('\n').map(Number);
+var lines = input.split('\n');
+
+let [code, qntd] = lines[0].split(" ").map(Number);
+
+let foods = {
+  1: 4.00,
+  2: 4.50,
+  3: 5.00,
+  4: 2.00,
+  5: 1.50,
+};
+
+if(foods.hasOwnProperty(code)){
+  console.log("Total: R$ " + (foods[code] * qntd).toFixed(2));
+};
 
 
+//console.log("Total: R$" + totalPagar.toFixed(2))
 
-/*
+/* 4, 4.50, 5, 2, 1.50
 Com base na tabela abaixo, escreva um programa que leia o código de um item e a quantidade deste item. A seguir, calcule e mostre o valor da conta a pagar.
 
 Entrada
