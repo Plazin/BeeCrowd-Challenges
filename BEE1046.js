627fe4d059eb4b03709dbc2cdee852fd69d67700
NandoPla <fernandopla8@gmail.com>
2023-03-17 23:04:02 -0300

    feat: new problems

--- BEE1046.js
+++ BEE1046.js
@@ -1,6 +1,21 @@
 var input = require('fs').readFileSync('./dev/stdin', 'utf8');
 var lines = input.split('\n');
 
+let [horaInicio, horaFinal] = lines[0].split(" ").map(Number);
+
+if(horaInicio == horaFinal){
+  console.log(`O JOGO DUROU 24 HORA(S)`)
+}
+
+if(horaInicio > horaFinal){
+  let horas = Math.abs(horaInicio - 24) + horaFinal
+  console.log(`O JOGO DUROU ${horas} HORA(S)`)
+} 
+
+if(horaInicio < horaFinal){
+  let horas = Math.abs(horaInicio - 24) - Math.abs(horaFinal - 24)
+  console.log(`O JOGO DUROU ${horas} HORA(S)`)
+}
 
 
 /* Leia a hora inicial e a hora final de um jogo. A seguir calcule a duração do jogo, sabendo que o mesmo pode começar em um dia e terminar em outro, tendo uma duração mínima de 1 hora e máxima de 24 horas.
