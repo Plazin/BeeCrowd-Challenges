16dced2089a12117209c3ffe0de896b6d9686f70
NandoPla <fernandopla8@gmail.com>
2023-03-18 21:24:04 -0300

    feat: new problems

--- BEE1047.js
+++ BEE1047.js
@@ -2,25 +2,65 @@ var input = require('fs').readFileSync('./dev/stdin', 'utf8');
 var lines = input.split('\n');
 
 let [horaInicial, minutoInicial, horaFinal, minutoFinal] = lines[0].split(" ").map(Number);
-// 7 8 9 10
-
-let horas = Math.abs(horaInicial - 24) - Math.abs(horaFinal - 24)
-let minutos = Math.abs(minutoInicial - 60) - Math.abs(minutoFinal - 60)
 
 if(horaInicial == horaFinal && minutoInicial == minutoFinal){
-  console.log(`O JOGO DUROU 24 HORA(S)`)
+  return console.log(`O JOGO DUROU 24 HORA(S) E 0 MINUTO(S)`)
+}
+
+if(horaInicial == horaFinal && minutoInicial < minutoFinal){
+  let horas = Math.abs(horaInicial - horaFinal)
+  let minutos = Math.abs(minutoInicial - minutoFinal)
+  return console.log(`O JOGO DUROU ${horas} HORA(S) E ${minutos} MINUTO(S)`)
+}
+
+if(horaInicial == horaFinal && minutoInicial > minutoFinal){
+  let horas = Math.abs(horaInicial - 24) + horaFinal - 1
+  let minutos = Math.abs(minutoInicial - 60) + minutoFinal
+  return console.log(`O JOGO DUROU ${horas} HORA(S) E ${minutos} MINUTO(S)`)
+}
+
+if(horaInicial < horaFinal && minutoInicial == minutoFinal){
+  let horas = Math.abs(horaInicial - horaFinal)
+  let minutos = Math.abs(minutoInicial -  minutoFinal)
+  return console.log(`O JOGO DUROU ${horas} HORA(S) E ${minutos} MINUTO(S)`)
+}
+
+if(horaInicial > horaFinal && minutoInicial == minutoFinal){
+  let horas = Math.abs(horaInicial - 24) + horaFinal
+  let minutos = Math.abs(minutoInicial - 60) + minutoFinal
+  return console.log(`O JOGO DUROU ${horas} HORA(S) E ${minutos} MINUTO(S)`)
 }
 
 if(horaInicial > horaFinal && minutoInicial > minutoFinal){
-  console.log(`O JOGO DUROU ${horas} HORA(S) E ${minutos} MINUTO(S)`)
+  let horas = Math.abs(horaInicial - 24) + horaFinal - 1
+  let minutos = Math.abs(minutoInicial - 60) + minutoFinal
+  return console.log(`O JOGO DUROU ${horas} HORA(S) E ${minutos} MINUTO(S)`)
 } 
 
 if(horaInicial < horaFinal && minutoInicial < minutoFinal){
-  console.log(`O JOGO DUROU ${horas} HORA(S) E ${minutos} MINUTO(S)`)
+  let horas = (Math.abs(horaInicial - 24) - Math.abs(horaFinal - 24));
+  let minutos = (Math.abs(minutoInicial - 60) - Math.abs(minutoFinal - 60));
+  return console.log(`O JOGO DUROU ${horas} HORA(S) E ${minutos} MINUTO(S)`)
 }
 
-console.log(horas)
-console.log(minutos)
+if(horaInicial > horaFinal && minutoInicial < minutoFinal){
+  let horas = Math.abs(horaInicial - horaFinal - 24) ;
+  let minutos = (Math.abs(minutoInicial - 60) - Math.abs(minutoFinal - 60));
+  return console.log(`O JOGO DUROU ${horas} HORA(S) E ${minutos} MINUTO(S)`)
+}
+
+if(horaInicial < horaFinal && minutoInicial > minutoFinal){
+  let horas = (Math.abs(horaInicial - 24) - Math.abs(horaFinal - 24)) - 1;
+  let minutos = (Math.abs(minutoInicial - 60) + minutoFinal)
+  return console.log(`O JOGO DUROU ${horas} HORA(S) E ${minutos} MINUTO(S)`)
+}
+
+/* if(horas <= 1 || minutos <= 1 && horas != 0 || minutos != 0){
+  let horas = (Math.abs(horaInicial - 24) - Math.abs(horaFinal - 24));
+  let minutos = (Math.abs(minutoInicial - 60) - Math.abs(minutoFinal - 60)) + Math.abs(60);
+  console.log(`O JOGO DUROU ${horas} HORA(S) E ${minutos} MINUTO(S)`)
+} */
+
 
 
 
