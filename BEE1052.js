ecff21420df0e34cb6fd16d4aec0e54e2d479244
NandoPla <fernandopla8@gmail.com>
2023-03-24 21:09:32 -0300

    feat: new problems

---
+++ BEE1052.js
@@ -0,0 +1,31 @@
+var input = require('fs').readFileSync('./dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+let mes = lines[0];
+
+let meses = {
+  1: "January",
+  2: "February",
+  3: "March",
+  4: "April",
+  5: "May",
+  6: "June",
+  7: "July",
+  8: "August",
+  9: "September",
+  10: "October",
+  11: "November",
+  12: "December"
+}
+
+if(meses.hasOwnProperty){
+  console.log(meses[mes])
+}
+
+/*Leia um valor inteiro entre 1 e 12, inclusive. Correspondente a este valor, deve ser apresentado como resposta o mês do ano por extenso, em inglês, com a primeira letra maiúscula.
+
+Entrada
+A entrada contém um único valor inteiro.
+
+Saída
+Imprima por extenso o nome do mês correspondente ao número existente na entrada, com a primeira letra em maiúscula.*/
