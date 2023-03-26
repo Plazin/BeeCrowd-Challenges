9af32e29c68f07afe7fa07d3739430b57cf693f3
NandoPla <fernandopla8@gmail.com>
2023-03-26 01:29:24 -0300

    feat: new problems

---
+++ BEE1059.js
@@ -0,0 +1,18 @@
+var input = require('fs').readFileSync('./dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+for(let i = 1; i >= 1 && i <= 100; i++){
+  if(i % 2 == 0){
+    console.log(i)
+  }
+};
+
+
+
+/*Faça um programa que mostre os números pares entre 1 e 100, inclusive.
+
+Entrada
+Neste problema extremamente simples de repetição não há entrada.
+
+Saída
+Imprima todos os números pares entre 1 e 100, inclusive se for o caso, um em cada linha.*/
