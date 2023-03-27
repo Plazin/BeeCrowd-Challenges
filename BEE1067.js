718f60551516eac68ef9ea39684dcdb023ab1028
NandoPla <fernandopla8@gmail.com>
2023-03-27 00:00:48 -0300

    feat: new problems

--- BEE1067.js
+++ BEE1067.js
@@ -3,8 +3,8 @@ var lines = input.split('\n');
 
 let x = Number(lines[0]);
 
-  for(let i = 1; i < x; i++){
-    if(x % 2 != 0){
+  for(let i = 1; i <= x; i++){
+    if(i % 2 != 0){
       console.log(i)
     }
   };
