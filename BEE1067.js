cb9b624c2248f4d69cef36018b7702cc7690152c
NandoPla <fernandopla8@gmail.com>
2023-03-26 23:53:46 -0300

    feat: new problems

---
+++ BEE1067.js
@@ -0,0 +1,18 @@
+var input = require('fs').readFileSync('./dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+let x = Number(lines[0]);
+
+  for(let i = 1; i < x; i++){
+    if(x % 2 != 0){
+      console.log(i)
+    }
+  };
+
+/*Leia um valor inteiro X (1 <= X <= 1000). Em seguida mostre os ímpares de 1 até X, um valor por linha, inclusive o X, se for o caso.
+
+Entrada
+O arquivo de entrada contém 1 valor inteiro qualquer.
+
+Saída
+Imprima todos os valores ímpares de 1 até X, inclusive X, se for o caso.*/
