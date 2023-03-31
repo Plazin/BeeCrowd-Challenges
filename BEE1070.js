a9629656a06778cb159c709f7fd5102d52955c9f
NandoPla <fernandopla8@gmail.com>
2023-03-30 22:27:16 -0300

    feat: new problems

---
+++ BEE1070.js
@@ -0,0 +1,10 @@
+var input = require('fs').readFileSync('./dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+/*Leia um valor inteiro X. Em seguida apresente os 6 valores ímpares consecutivos a partir de X, um valor por linha, inclusive o X ser for o caso.
+
+Entrada
+A entrada será um valor inteiro positivo.
+
+Saída
+A saída será uma sequência de seis números ímpares.*/
