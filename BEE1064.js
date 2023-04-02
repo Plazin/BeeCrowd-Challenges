5fde8a2c3131cd24f9ff237647d2e43d4ee77eaa
NandoPla <fernandopla8@gmail.com>
2023-04-02 19:51:06 -0300

    feat: new problems

--- BEE1064.js
+++ BEE1064.js
@@ -1,41 +1,22 @@
 var input = require('fs').readFileSync('./dev/stdin', 'utf8');
-var lines = input.split('\n').map(Number);;
+var lines = input.split('\n').map(Number);
 
-let entrada = lines[0];
+let entrada = lines;
+let numerosPositivos = [];
+let somaPositivos;
 
-let valor1 = lines[0];
-let valor2 = lines[1];
-let valor3 = lines[2];
-let valor4 = lines[3];
-let valor5 = lines[4];
-let valor6 = lines[5];
-
-let arr = [valor1, valor2, valor3, valor4, valor5, valor6]
-let resultado = [];
-let media = resultado / resultado.length
-
-console.log(arr)
-
-if(valor1 > 0){
-  resultado.push(valor1)
-} 
- if(valor2 > 0){
-  resultado.push(valor2)
-} 
- if(valor3 > 0){
-  resultado.push(valor3)
-} 
- if(valor4 > 0){
-  resultado.push(valor4)
-} 
- if(valor5 > 0){
-  resultado.push(valor5)
-} 
- if(valor6 > 0){
-  resultado.push(valor6)
+for(let i = 0; i < entrada.length; i++){
+  if(entrada[i] > 0){
+    numerosPositivos.push(entrada[i])
+    somaPositivos = numerosPositivos.reduce((a, b) => a + b);
+  }
 }
-console.log(`${resultado.length} valores positivos`)
-console.log(`${media.toFixed(1)}`)
+
+console.log(`${numerosPositivos.length} valores positivos`)
+console.log(somaPositivos / numerosPositivos.length)
+
+/* `${entrada[i].length} números positivos`
+console.log(`${numerosPositivos.length} numeros positivos`) */
 
 /* Leia 6 valores. Em seguida, mostre quantos destes valores digitados foram positivos. Na próxima linha, deve-se mostrar a média de todos os valores positivos digitados, com um dígito após o ponto decimal.
 
