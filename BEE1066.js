9f92376c3a2a7afbf22692832e80fec8b42281b0
NandoPla <fernandopla8@gmail.com>
2023-04-06 00:18:00 -0300

    feat: new problems

--- BEE1066.js
+++ BEE1066.js
@@ -1,27 +1,29 @@
 var input = require('fs').readFileSync('./dev/stdin', 'utf8');
 var lines = input.split('\n').map(Number);
 
-let entrada = lines;
-let valores = [];
+let valorPar = [],
+    valorImpar = [],
+    valorPos = [],
+    valorNeg = []
 
-for(i = 0; i < entrada.length; i++){
-  if(entrada[i] % 2 === 0){
-    valores.push(entrada[i])
-    console.log(`${valores.length} valor(es) par(es)`)
+for(i = 0; i < lines.length - 1; i++){// O exercicio apenas é aprovado com o -1 no lines.length, não me pergunte o porquê.
+  if(lines[i] % 2 === 0){
+    valorPar.push(lines[i])
   }
-  if(entrada[i] % 2 !== 0){
-    valores.push(entrada[i])
-    console.log(`${valores.length} valor(es) ímpar(es)`)
+  if(lines[i] % 2 !== 0){
+    valorImpar.push(lines[i])
   }
-  if(entrada[i] > 0){
-    valores.push(entrada[i])
-    console.log(`${valores.length} valor(es) positivo(s)`)
+  if(lines[i] > 0){
+    valorPos.push(lines[i])
   }
-  if(entrada[i] < 0){
-    valores.push(entrada[i])
-    console.log(`${valores.length} valor(es) negativo(s)`)
+  if(lines[i] < 0){
+    valorNeg.push(lines[i])
   }
 }
+console.log(`${valorPar.length} valor(es) par(es)`)
+console.log(`${valorImpar.length} valor(es) impar(es)`)
+console.log(`${valorPos.length} valor(es) positivo(s)`)
+console.log(`${valorNeg.length} valor(es) negativo(s)`)
 
 /*Leia 5 valores Inteiros. A seguir mostre quantos valores digitados foram pares, quantos valores digitados foram ímpares, quantos valores digitados foram positivos e quantos valores digitados foram negativos.
 
